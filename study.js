3f0c0c2176c172d2aa1a89fab104dd0f214287ca
yara <yaraeb121@gmail.com>
2022-03-11 19:19:51 +0200

    style updates

--- study.js
+++ study.js
@@ -30,6 +30,11 @@ function showSlides(n) {
 }
 
 
-.img-five{
-
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
